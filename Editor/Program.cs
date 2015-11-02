f1b71562f772c6af140cb9f097338dcbeb927d5d
Hesham Meneisi <heshammeneisi@gmail.com>
2015-11-02 22:37:40 +0200

    1.2F

--- Editor/Program.cs
+++ Editor/Program.cs
@@ -52,15 +52,16 @@ namespace ASMSharp
             throw (ex);
 #endif
             string sep = "\r\n\r\n####################\r\n\r\n";
-            string data = type + sep + ex.Message + sep + ex.StackTrace;
+            string data = "Want to help? Kindly email this file to heshammeneisi@gmail.com along with some details of what operation caused the error.\n Further more, you can check the code yourself on github.com/HeshamMeneisi/ASMSharp\n" + sep + type + sep + ex.Message + sep + ex.StackTrace;
             StreamWriter sw = new StreamWriter("ASLog.txt");
             sw.WriteLine(data);
             sw.Close();
-            MessageBox.Show("The program encountered a problem and has to stop.\nERROR:" + ex.Message + "\nFor more information refer to the ASLog.txt file.", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            MessageBox.Show("The program encountered a problem and has to restart.\nYou should find your last running code in \"" + Environment.CurrentDirectory + "\\SRCFILE\"\nERROR:" + ex.Message + "\nFor more information refer to the ASLog.txt file.", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
             Process p = new Process();
             p.StartInfo.FileName = "explorer.exe";
             p.StartInfo.Arguments = Path.Combine(Environment.CurrentDirectory, "ASLog.txt");
             p.Start();
+            Application.Restart();
         }
     }
 }
